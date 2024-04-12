4aef809c24bf95aea5e018abefaafb950cbcb31f
nomorechokedboy <haiduongit@protonmail.com>
2024-04-12 11:18:23 +0700

    feat: implement registrations operations

--- apps/api/internal/registrations/handler.go
+++ apps/api/internal/registrations/handler.go
@@ -9,6 +9,10 @@ type RegistrationHandler struct {
 func (h *RegistrationHandler) RegisterRoute(group fiber.Router) {
 	r := group.Group("/registrations")
 	r.Get("/", h.Find)
+	r.Get("/:id<int,min(1)>", h.FindOne)
+	r.Post("/", h.Insert)
+	r.Put("/:id<int,min(1)>", h.Update)
+	r.Delete("/:id<int,min(1)>", h.Delete)
 }
 
 func New(repo RegistrationRepository) *RegistrationHandler {
