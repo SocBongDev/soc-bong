77d8b260d3a8f1c1fa00e620908e7d7200833be2
nomorechokedboy <haiduongit@protonmail.com>
2024-04-17 09:33:28 +0700

    feat(classes): apply authz

--- apps/api/internal/classes/handler.go
+++ apps/api/internal/classes/handler.go
@@ -2,6 +2,7 @@ package classes
 
 import (
 	"github.com/SocBongDev/soc-bong/internal/common"
+	"github.com/SocBongDev/soc-bong/internal/middlewares"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -11,10 +12,10 @@ type ClassHandler struct {
 
 func (h *ClassHandler) RegisterRoute(group fiber.Router) {
 	r := group.Group("/classes")
-	r.Get("/", h.Find)
-	r.Get("/:id<int,min(1)>", h.FindOne)
-	r.Post("/", h.Insert)
-	r.Put("/:id<int,min(1)>", h.Update)
+	r.Get("/", middlewares.ValidatePermissions([]string{"read:classes"}), h.Find)
+	r.Get("/:id<int,min(1)>", middlewares.ValidatePermissions([]string{"read:classes"}), h.FindOne)
+	r.Post("/", middlewares.ValidatePermissions([]string{"create:classes"}), h.Insert)
+	r.Put("/:id<int,min(1)>", middlewares.ValidatePermissions([]string{"update:classes"}), h.Update)
 }
 
 func New(repo ClassRepository) common.APIHandler {
