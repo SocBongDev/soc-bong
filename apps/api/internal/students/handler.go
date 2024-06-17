58c2a67bc51a1700a921a9a78289243696527e36
tinhau1204 <tinhau1204@gmail.com>
2024-06-17 11:49:26 +0700

    fix(students): delete parent model add parent field into students model change all the usecase

--- apps/api/internal/students/handler.go
+++ apps/api/internal/students/handler.go
@@ -2,35 +2,21 @@ package students
 
 import (
 	"github.com/SocBongDev/soc-bong/internal/common"
-	"github.com/SocBongDev/soc-bong/internal/parents"
 	"github.com/gofiber/fiber/v2"
 )
 
 type StudentHandler struct {
-	repo       StudentRepository
-	parentRepo parents.ParentRepository
+	repo StudentRepository
 }
 
 func (h *StudentHandler) RegisterRoute(router fiber.Router) {
 	r := router.Group("/students")
-<<<<<<< HEAD
-
-	r.Get(
-		"/:id<int,min(1)>",
-		h.FindOne,
-	)
-
-	r.Get(
-		"/",
-		h.Find,
-	)
-=======
 	r.Get("/", h.Find)
 	r.Get("/:id<int,min(1)>", h.FindOne)
 	r.Post("/", h.Insert)
->>>>>>> d34fe274eb4f889099a7833b941bcaf0c65b76cd
+	r.Put("/:id<int,min(1)>", h.Update)
 }
 
-func New(repo StudentRepository, parentRepo parents.ParentRepository) common.APIHandler {
-	return &StudentHandler{repo: repo, parentRepo: parentRepo}
+func New(repo StudentRepository) common.APIHandler {
+	return &StudentHandler{repo: repo}
 }
