c8f9e780ccf2e2dc928c5f32ef82bc387c65691b
tinhau1204 <tinhau1204@gmail.com>
2024-06-29 21:35:28 +0700

    feat(auth0): set up session and check authenticated session

---
+++ apps/web/src/routes/(protected)/admin/+layout.ts
@@ -0,0 +1,17 @@
+import { getSession, isAuthenticated } from "$lib/services/auth";
+import { redirect } from "@sveltejs/kit";
+
+export async function load({ params, parent, url, data }) {
+    if (url.pathname.startsWith("/")) {
+        const session = getSession();
+        if (session) {
+            if (isAuthenticated()) {
+                return {};
+            }
+
+            throw redirect(303, "/");
+        }
+
+        throw redirect(303, "/");
+    }
+}
