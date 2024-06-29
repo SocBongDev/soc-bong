21399a7aaedf49352783522f64198ea4428cb4f3
tinhau1204 <tinhau1204@gmail.com>
2024-06-29 15:00:14 +0700

    feat(auth0): add access token for agencies api request

--- apps/web/src/routes/(protected)/admin/agencies/+page.ts
+++ apps/web/src/routes/(protected)/admin/agencies/+page.ts
@@ -1,17 +1,24 @@
 import type { AgencyProps } from '../type'
 import type { PageLoad } from './$types'
-
-const API_URL = 'http://127.0.0.1:5000/api/v1'
-
+import { PUBLIC_API_SERVER_URL } from "$env/static/public"
 
 export const load: PageLoad = async ({ fetch, url, depends }) => {
 	const page = Number(url.searchParams.get('page') || '1')
 	const pageSize = Number(url.searchParams.get('pageSize') || '15')
 	const query = new URLSearchParams()
+	const token = localStorage.getItem("access_token");
 	query.set('page', String(page))
 	query.set('pageSize', String(pageSize))
 
-	const res = await fetch(`${API_URL}/agencies?${query}`)
+	const res = await fetch(`${PUBLIC_API_SERVER_URL}/agencies?${query}`,
+		{
+			method: "GET",
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json",
+			}
+		}
+	)
 	depends('app:agencies')
 
 	return {
