09f3aa3deef229ecc5731b974eb8919b44af26a2
tinhau1204 <tinhau1204@gmail.com>
2024-06-29 22:02:01 +0700

    feat(auth/api): add access token to API request and add classId query param for fetch data from students API

--- apps/web/src/routes/(protected)/admin/students/+page.ts
+++ apps/web/src/routes/(protected)/admin/students/+page.ts
@@ -1,21 +1,43 @@
-import type { Agency, Student } from '$lib'
+import type { ClassesProps, AgencyProps, StudentProps } from '../type'
 import type { PageLoad } from './$types'
+import { PUBLIC_API_SERVER_URL } from "$env/static/public"
 
 export const load: PageLoad = async ({ fetch, url, depends }) => {
 	const page = Number(url.searchParams.get('page') || '1')
 	const pageSize = Number(url.searchParams.get('pageSize') || '15')
+	const classId = Number(url.searchParams.get('classId') || '1')
 	const query = new URLSearchParams()
+	const token = localStorage.getItem("access_token")
 	query.set('page', String(page))
 	query.set('pageSize', String(pageSize))
 
-	const res = await fetch(`/api/students?${query}`)
-	const agencyRes = await fetch(`/api/agencies?${query}`)
+	const res = await fetch(`${PUBLIC_API_SERVER_URL}/students?classId=${classId}&${query}`, {
+		method: "GET",
+		headers: {
+			"Authorization": `Bearer ${token}`,
+			"Content-Type": "application/json",
+		}
+	})
+	const agencyRes = await fetch(`${PUBLIC_API_SERVER_URL}/agencies?${query}`, {
+		method: "GET",
+		headers: {
+			"Authorization": `Bearer ${token}`,
+			"Content-Type": "application/json",
+		}
+	})
+	const classes = await fetch(`${PUBLIC_API_SERVER_URL}/classes?${query}`, {
+		method: "GET",
+		headers: {
+			"Authorization": `Bearer ${token}`,
+			"Content-Type": "application/json",
+		}
+	})
 
 	depends('app:students')
-	depends('app:agencies')
 
 	return {
-		students: res.json() as Promise<{ page: number; pageSize: number; data: Student[] }>,
-		agencies: agencyRes.json() as Promise<{ page: number; pageSize: number; data: Agency[] }>
+		students: res.json() as Promise<{ page: number; pageSize: number; data: StudentProps[] }>,
+		agencies: agencyRes.json() as Promise<{ page: number; pageSize: number; data: AgencyProps[] }>,
+		classes: classes.json() as Promise<{ page: number; pageSize: number; data: ClassesProps[] }>
 	}
 }
