11fb7356efd0ab5b585d07caac162caff6c48bd9
tinhau1204 <tinhau1204@gmail.com>
2024-06-29 14:50:56 +0700

    fix(auth0): delete supabase

--- apps/web/src/app.d.ts
+++ apps/web/src/app.d.ts
@@ -1,24 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-import type { RegistrationRepository, StudentRepository, ParentRepository, AgencyRepository } from '$lib'
 import 'unplugin-icons/types/svelte'
 
 declare global {
 	namespace App {
 		// interface Error {}
-		interface Locals {
-			supabase: SupabaseClient<Database>
-			registrationRepo: RegistrationRepository
-			studentRepo: StudentRepository
-			parentRepo: ParentRepository
-			agencyRepo: AgencyRepository
-			getSession(): Promise<Session | null>
-		}
-		interface PageData {
-			session: Session | null
-		}
+		// interface Locals {}
+		// interface PageData {}
 		// interface Platform {}
 	}
 }
 
-export {}
+export { }
