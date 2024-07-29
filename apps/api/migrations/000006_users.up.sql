22c5a7dc45dc9e34d1cbe24e1ae7f50d5011cbab
tinhau1204 <tinhau1204@gmail.com>
2024-07-29 11:14:42 +0700

    feat(users): add migrations users

---
+++ apps/api/migrations/000006_users.up.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+
+    email TEXT NOT NULL UNIQUE,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    is_active BOOLEAN DEFAULT FALSE NOT NULL,
+    verify_email BOOLEAN DEFAULT FALSE NOT NULL,
+    dob DATE NOT NULL,
+    phone_number TEXT NOT NULL,
+    connection TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    
+    agency_id INTEGER DEFAULT 1 NOT NULL,
+
+    FOREIGN KEY (agency_id) REFERENCES agencies(id)
+);
+
+CREATE INDEX idx_users_email ON users(email);
+
+CREATE TRIGGER update_timestamp_users AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+    UPDATE users SET updated_at = CURRENT_TIMESTAMP WHERE id = OLD.id;
+END;
