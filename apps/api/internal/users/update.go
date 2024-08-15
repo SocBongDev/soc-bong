299f6cb44046880dde1d0b3f802d18d68fb1d9c5
tinhau1204 <tinhau1204@gmail.com>
2024-08-15 17:53:18 +0700

    feat(users): add update method for udpate user information to database

---
+++ apps/api/internal/users/update.go
@@ -0,0 +1,44 @@
+package users
+
+import (
+	"log"
+
+	"github.com/SocBongDev/soc-bong/internal/common"
+	"github.com/gofiber/fiber/v2"
+)
+
+// @UpdateUser godoc
+// @Summary Update user api
+// @Description Update user
+// @Tags User
+// @Accept json
+// @Param post body UserInput true "Update user body"
+// @Param id path int true "User ID"
+// @Success 200 {object} User
+// @Failure 500 {string} string
+// @Security ApiKeyAuth
+// @Router /users/{id} [put]
+func (h *UserHandler) Update(c *fiber.Ctx) error {
+	body := new(UserInput)
+	if err := c.BodyParser(body); err != nil {
+		log.Println("UpdateUser.BodyParser err: ", err)
+		return fiber.ErrBadRequest
+	}
+
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		log.Println("UpdateUser.ParamsInt err: ", err)
+		return fiber.ErrBadRequest
+	}
+
+	log.Printf("UpdateRegistration request: %+v\n", body)
+	req := &User{UserInput: *body, BaseEntity: common.BaseEntity{Id: id}}
+	if err := h.repo.Update(req); err != nil {
+		log.Println("UpdateRegistration.Update err: ", err)
+		return fiber.ErrInternalServerError
+	}
+
+	log.Printf("UpdateUser success. Response: %+v\n", body)
+
+	return c.JSON(req)
+}
