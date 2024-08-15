43ee6c0bc4477a12329dd57ca8aaf2788902eebd
tinhau1204 <tinhau1204@gmail.com>
2024-08-15 18:09:28 +0700

    chore: fix pagination direct url

--- apps/web/src/routes/(protected)/admin/registrations/+page.svelte
+++ apps/web/src/routes/(protected)/admin/registrations/+page.svelte
@@ -462,7 +462,7 @@
 					class={data.registrations.page === 1
 						? 'pointer-events-none cursor-default opacity-40'
 						: ''}
-					href={`/admin?page=${data.registrations.page - 1}&pageSize=${
+					href={`/admin?registrations?page=${data.registrations.page - 1}&pageSize=${
 						data.registrations.pageSize
 					}`}
 				>
@@ -474,7 +474,7 @@
 					data.registrations.data.length === 0
 						? 'pointer-events-none cursor-default opacity-40'
 						: ''}
-					href={`/admin?page=${data.registrations.page + 1}&pageSize=${
+					href={`/admin/registrations?page=${data.registrations.page + 1}&pageSize=${
 						data.registrations.pageSize
 					}`}
 				>
