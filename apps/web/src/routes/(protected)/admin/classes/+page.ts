496e40b2e272f8e141accdd72cbd8842690875e4
tinhau1204 <tinhau1204@gmail.com>
2024-08-15 18:03:33 +0700

    feat(classes): add users for get users information in classes route

--- apps/web/src/routes/(protected)/admin/classes/+page.ts
+++ apps/web/src/routes/(protected)/admin/classes/+page.ts
@@ -1,7 +1,7 @@
 
 import type { PageLoad } from './$types'
 import { PUBLIC_API_SERVER_URL } from "$env/static/public"
-import type { AgencyProps, ClassesProps } from '$lib/common/type'
+import type { AgencyProps, ClassesProps, UserProps } from '$lib/common/type'
 
 export const load: PageLoad = async ({ fetch, url, depends }) => {
     const page = Number(url.searchParams.get('page') || '1')
@@ -31,10 +31,21 @@ export const load: PageLoad = async ({ fetch, url, depends }) => {
             }
         }
     )
+
+    const users = await fetch(`${PUBLIC_API_SERVER_URL}/users?${query}`, 
+        {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            }
+        }
+    )
     depends('app:classes')
 
     return {
         classes: classes.json() as Promise<{ page: number; pageSize: number; data: ClassesProps[] }>,
-        agencies: agencies.json() as Promise<{page: number; pageSize: number; data: AgencyProps[]}>
+        agencies: agencies.json() as Promise<{page: number; pageSize: number; data: AgencyProps[]}>,
+        users: users.json() as Promise<{page: number, pageSize: number, data: UserProps[]}>
     }
 }
