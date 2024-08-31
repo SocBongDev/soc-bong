206510d298867106e53687800ef64c484d3b27fa
tinhau1204 <tinhau1204@gmail.com>
2024-08-31 11:25:13 +0700

    fix(route): divine public and private route into separate route

--- apps/api/cmd/serve/app.go
+++ apps/api/cmd/serve/app.go
@@ -80,7 +80,8 @@ func (a *App) ApiV1(api fiber.Router) {
 		registrations.New(registrationRepo),
 		agencies.New(agencyRepo),
 	}
-	a.RegisterAPIHandlers(v1, publicHandlers)
+	publicGroup := v1.Group("/public")
+	a.RegisterAPIHandlers(publicGroup, publicHandlers)
 
 	v1.Use(middlewares.ValidateJWT(a.config.Audience, a.config.Domain))
 
