2ee2f72f02376087a0b74e233a58b46f56025ecb
tinhau1204 <tinhau1204@gmail.com>
2024-09-07 10:35:43 +0700

    feat(public-endpoint): add 2 more public route endpoints

--- apps/api/cmd/serve/app.go
+++ apps/api/cmd/serve/app.go
@@ -90,7 +90,9 @@ func (a *App) ApiV1(api fiber.Router) {
 	excel := spreadsheet.New()
 
 	publicEndpoints := map[string][9]string{
-		"/api/v1/agencies": {fiber.MethodGet},
+		"/api/v1/agencies":      {fiber.MethodGet},
+		"/api/v1/registrations": {fiber.MethodPost},
+		"/api/v1/sign-up":       {fiber.MethodPost},
 	}
 	skipJWTOption := func(c *fiber.Ctx) bool {
 		val := publicEndpoints[c.Path()]
