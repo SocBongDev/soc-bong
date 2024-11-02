61b868bf988f1da917690cb17c9b5fa3f28605f4
tinhau1204 <tinhau1204@gmail.com>
2024-11-02 23:49:50 +0700

    chore: add Viet name for label input

--- apps/web/src/lib/components/PasswordField.svelte
+++ apps/web/src/lib/components/PasswordField.svelte
@@ -2,6 +2,7 @@
 	import Error from './FloatingLabel/Error.svelte'
 
 	export let name: string
+	export let viName: string
 	export let required = false
 	export let value: string | undefined = undefined
 	export let error: string[] | null = null
@@ -37,7 +38,7 @@
 					? "-ml-px -mt-0.5 after:text-xs after:text-red-500 after:content-['_*']"
 					: ''}"
 			>
-				{name}
+				{viName}
 			</span>
 		</label>
 		<button
